21125d44d69c6eaebb1d4ffc56271363b5fba7f3
Sacura-ss <sacura-ss@yandex.ru>
2022-06-05 16:11:49 +0300

    Add Library Activity

--- app/src/main/java/com/example/bookvibe/LibraryActivity.java
+++ app/src/main/java/com/example/bookvibe/LibraryActivity.java
@@ -5,6 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.View;
 
 import com.example.bookvibe.adapters.AdapterPdfUser;
@@ -19,6 +22,7 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 
 /*public class LibraryActivity extends AppCompatActivity {
@@ -120,12 +124,18 @@ import java.util.List;
 
 public class LibraryActivity extends AppCompatActivity {
 
-
     //view binding
     private ActivityLibraryBinding binding;
 
+    //arraylist to hold list of data of type modelPdf
+    private List<ModelPdf> pdfArrayList;
+    //adapter
+    private AdapterPdfUser adapterPdfUser;
+
     //firebase auth
     private FirebaseAuth firebaseAuth;
+
+    private static final String TAG = "PDF_LIST_TAG";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -136,8 +146,84 @@ public class LibraryActivity extends AppCompatActivity {
         //init firebase auth
         firebaseAuth = FirebaseAuth.getInstance();
 
+        loadPdfList();
 
+        //search
+        binding.searchEt.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
 
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                //search as and when type
+                try {
+                    adapterPdfUser.getFilter().filter(charSequence);
+                } catch (Exception e) {
+                    Log.d(TAG, "onTextChanged: "+e.getMessage());
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        //handle click, go to previous action
+        binding.backBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onBackPressed();
+            }
+        });
     }
 
+    private void loadPdfList() {
+        //init list
+        pdfArrayList = new ArrayList<>();
+
+        DatabaseReference reference = FirebaseDatabase.getInstance().getReference("Users");
+        reference.child(firebaseAuth.getUid()).child("Library")
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        pdfArrayList.clear();
+                        for(DataSnapshot dataSnapshot: snapshot.getChildren()) {
+                            HashMap<String,String> values = (HashMap<String, String>) dataSnapshot.getValue();
+                            String bookId = values.get("bookId");
+
+                            DatabaseReference reference1 = FirebaseDatabase.getInstance().getReference("Books");
+                            reference1.orderByChild("id").equalTo(bookId)
+                                    .addValueEventListener(new ValueEventListener() {
+                                        @Override
+                                        public void onDataChange(@NonNull DataSnapshot snapshot) {
+                                            for(DataSnapshot dataSnapshot1: snapshot.getChildren()) {
+                                                //get data
+                                                ModelPdf model = dataSnapshot1.getValue(ModelPdf.class);
+                                                //add to list
+                                                pdfArrayList.add(model);
+
+                                                Log.d(TAG, "onDataChange: "+model.getId()+" "+model.getTitle());
+                                            }
+                                        }
+                                        @Override
+                                        public void onCancelled(@NonNull DatabaseError error) {
+
+                                        }
+                                    });
+                        }
+                        //setup adapter
+                        adapterPdfUser = new AdapterPdfUser(LibraryActivity.this, pdfArrayList);
+                        binding.bookRv.setAdapter(adapterPdfUser);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
+        //6 55:50
+    }
 }
